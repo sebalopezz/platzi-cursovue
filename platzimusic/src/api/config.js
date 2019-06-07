16704df8ef08aa802da2c09a9e913dc5bbdf009d
sebalopezz <jorgesebastianlopez@gmail.com>
2019-06-07 11:46:24 -0300

    configurando api

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: 'e0c2fb93f48808570081ac0bd187814b',
+    secret: 'f67ba63418706220b0aaa45d76afb8cf',
+    registeredTo: 'sebalopez98'
+}
+
+// Application name	platzimusic
+// API key	e0c2fb93f48808570081ac0bd187814b
+// Shared secret	f67ba63418706220b0aaa45d76afb8cf
+// Registered to	sebalopez98
